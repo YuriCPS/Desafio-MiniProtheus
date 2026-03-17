e296dd228970afd9ad6956baa9586ec151593ed5
Yuri Carvalho Peixoto <yuricps@gmail.com>
2026-03-17 14:49:25 -0300

    [FEAT] - Cria o service dos produtos com o CRUD básico.

---
+++ MiniProtheus.Server/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+using MiniProtheus.Server.DTOs;
+
+namespace MiniProtheus.Server.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductResponseDto>> GetAllAsync();
+        Task<ProductResponseDto?> GetByIdAsync(int id);
+        Task<ProductResponseDto> CreateAsync(CreateProductDto dto);
+        Task<ProductResponseDto?> UpdateAsync(int id, UpdateProductDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
