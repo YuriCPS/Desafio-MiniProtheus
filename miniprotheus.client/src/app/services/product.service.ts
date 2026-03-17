269c28129e170531823a2c7b044504d65915e73f
Yuri Carvalho Peixoto <yuricps@gmail.com>
2026-03-17 16:49:08 -0300

    [FEAT] - Adiciona componente da listagem de produtos

---
+++ miniprotheus.client/src/app/services/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product, CreateProduct, UpdateProduct } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private readonly apiUrl = '/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  getById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  create(product: CreateProduct): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+
+  update(id: number, product: UpdateProduct): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
